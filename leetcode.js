bfba2ee103959291099f9b8f8e19e0710a956323
Roberto Artigues <rartigues2019@inf.udec.cl>
2021-12-17 18:41:07 -0300

    edit

---
+++ leetcode.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+    var str = x.toString();
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+};
--- test.js
+++ test.js
@@ -1,2 +1,30 @@
-const prompt = require('prompt-sync')();
 
+
+let input = '( () ( () ) () )';
+
+let valido=true;
+let valor;
+let stack= []
+for (let i = 0; i < input.length; i++) {
+    if(input[i]=='(' || input[i]==')') valor= input[i];
+    else continue;
+    if(valor== '(') {
+        stack.push(input[i]);
+        console.log(stack);
+    } else {
+        //')'?
+        if(stack.length == 0) {
+            valido=false; //HABIA ) PERO NO ( malo
+            break;
+        }
+        else {
+            stack.pop();
+        }
+
+    }
+
+}
+
+if(stack.length == 0) valido=false;
+if(valido) console.log('Balanceado')
+else console.log('No balanceado')
